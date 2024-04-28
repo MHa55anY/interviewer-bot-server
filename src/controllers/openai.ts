1a246cd4e1774e0f21821ceb25c170e022dcea4b
Hassan Yusuf <mhyyusuf3@gmail.com>
2024-04-28 22:08:25 +0530

    Controller for open ai features

---
+++ src/controllers/openai.ts
@@ -0,0 +1,73 @@
+import OpenAI from "openai";
+import fs from 'fs';
+import path from "path";
+import isFileEmpty from "../helper/isFileEmpty";
+
+const openai = new OpenAI({apiKey: process.env.OPENAI_KEY});
+
+//Speech to text
+export async function transcribeSpeechAndPushToHistory() {
+    try {
+        const transcription = await openai.audio.transcriptions.create({
+            file: fs.createReadStream("./test.mp3"),
+            model: "whisper-1",
+          });
+        
+        console.log(transcription.text);
+        pushToChatHistory({
+            role: 'user',
+            content: transcription.text
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+//send response to chatgpt
+const databasePath = path.join(__dirname, '../../database.json');
+const isDbEmpty = isFileEmpty(databasePath);
+const getChatHistory = () => {
+    const messages:OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [];
+    let jsonData;
+    if (!isDbEmpty) {
+      try {
+        const data = fs.readFileSync(databasePath, 'utf-8');
+        jsonData = JSON.parse(data);
+      } catch (error) {
+        console.log(error);
+      }
+      messages.push(...jsonData);
+    }
+    else {
+      messages.push({
+        "role": "system", "content": "Act as an interviewer for a Full Stack Developer position. Ask short questions that are relevant to a senior level developer. Your name is John. The user is Hassan. Start with an intro to yourself. Keep your responses under 30 words and be funny sometimes."
+      })
+    }
+    return messages;
+  };
+
+const pushToChatHistory = (message: OpenAI.Chat.Completions.ChatCompletionMessageParam) => {
+    const messages = getChatHistory();
+    try {
+        fs.writeFileSync(databasePath, Buffer.from(JSON.stringify([...messages, message])), 'utf-8');
+        console.log("File written successfully");     
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+// Open ai chatbot
+export async function talkToGpt() {
+    try {
+        const completion = await openai.chat.completions.create({
+            messages: getChatHistory(),
+            model: "gpt-3.5-turbo",
+            });
+            console.log(completion.choices);
+            pushToChatHistory(completion.choices[0].message);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default openai;

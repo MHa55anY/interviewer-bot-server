f385e10e24cc6b33aa9d8660911da7f4a80fba3b
Hassan Yusuf <mhyyusuf3@gmail.com>
2024-04-28 17:03:06 +0530

    Save incoming audio buffer to file

--- src/wsController.ts
+++ src/wsController.ts
@@ -6,12 +6,19 @@ const startWebSocketServer = () => {
     const wss = new WebSocket.Server({ port: 8081 });
 
     wss.on('connection', (ws) => {
+        console.log("client connected!");
         const audioStream = fs.readFileSync(path.join(__dirname, "../speech.mp3"));
         ws.send(audioStream);
+
+        ws.addEventListener("message", (event) => {
+            const audioBuffer = event.data as Buffer;
+            const speechFile = path.resolve("./test.mp3");
+            fs.writeFileSync(speechFile, audioBuffer);
+        });
   
-      ws.on('close', () => {
-        console.log('Client disconnected');
-      });
+        ws.on('close', () => {
+            console.log('Client disconnected');
+        });
     });
 };
 

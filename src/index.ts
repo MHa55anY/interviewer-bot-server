3727a754598dad252993939a83f70a7418328a24
Hassan Yusuf <mhyyusuf3@gmail.com>
2024-05-09 03:39:58 +0530

    modify /init-bot to behave differently for guest and host

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import express from 'express';
 import startWebSocketServer, { webSocket } from './controllers/websocket';
 import cors from 'cors';
 import { talkToGpt, textToSpeech } from './controllers/openai';
+import { exampleUsage } from './controllers/100ms';
+import puppeteer from 'puppeteer';
+import startBot from './controllers/bot';
+
+let count = 0;
 
 const app = express();
 app.use(cors());
@@ -14,12 +19,19 @@ app.post("/talk", (req,res) => {
   res.send("hello world");
 });
 
-app.get("/init-bot", async (_, res) => {
-  const textResponse = await talkToGpt();
-  if(textResponse) {
-    const res = await textToSpeech(textResponse);
-    if(res) webSocket.send(res);
-  };
+app.get("/init-bot", async (req, res) => {
+  const role: string = req.query.role as string;
+  if(role == 'host') {
+    console.log(role);
+    const textResponse = await talkToGpt();
+    if(textResponse) {
+      const speech = await textToSpeech(textResponse);
+      if(speech) webSocket.send(speech);
+    };
+  }
+  if(role === 'guest') {
+    await startBot();
+  }
   res.send({message: "Bot initialsed!"});
 });
 

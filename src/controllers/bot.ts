1e14a41032e52bbb894a3c945d756bda4d4a2a8f
Hassan Yusuf <mhyyusuf3@gmail.com>
2024-05-11 16:04:42 +0530

    Remove the error overlay in dev env

--- src/controllers/bot.ts
+++ src/controllers/bot.ts
@@ -1,15 +1,26 @@
-import puppeteer from "puppeteer";
+import puppeteer, { ElementHandle } from "puppeteer";
 
+//EDIT template according to client code
 const startBot = async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: ['--use-fake-ui-for-media-stream'], // Use fake UI for media stream
+    });
     const page = await browser.newPage();
-    await page.goto('http://localhost:3000');
-    // Perform further actions...
-    // Fill out the form fields
-    await page.type('#userName', 'Bot'); // Replace 'Your Name' with the desired value for the userName field
-    await page.type('#roomCode', 'rqv-crdr-lrl');
-    
-    await page.click('button.btn-primary');
+    await page.goto(`http://localhost:3001?roomcode=rqv-crdr-lrl`);
+
+      // Wait for the iframe to be loaded - For removing the error screen in dev mode
+    const iframe = await page.waitForSelector('iframe#webpack-dev-server-client-overlay') as ElementHandle<HTMLIFrameElement>;
+    const frame = await iframe.contentFrame();
+
+    const button = await frame.waitForSelector('button');
+    await button?.click();
+
+    await page.waitForSelector('#name');
+    await page.type('#name', 'Bot');
+    // await page.type('#roomCode', 'rqv-crdr-lrl');
+
+    await page.waitForSelector('#submit');
+    await page.click('#submit');
 };
 
 export default startBot;

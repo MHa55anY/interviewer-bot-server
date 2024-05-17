a4b9683bc480c108a448cf9d5a1aca8864b1465d
Hassan Yusuf <mhyyusuf3@gmail.com>
2024-05-18 03:15:45 +0530

    remove unnecessary endpoints

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,10 @@ import express from 'express';
 import startWebSocketServer, { webSocket } from './controllers/websocket';
 import cors from 'cors';
 import { talkToGpt, textToSpeech } from './controllers/openai';
-import { exampleUsage } from './controllers/100ms';
-import puppeteer from 'puppeteer';
 import startBot from './controllers/bot';
-
-let count = 0;
 
 const app = express();
 app.use(cors());
-
-app.get("/", (req,res) => {
-  res.send("hello world");
-})
-
-app.post("/talk", (req,res) => {
-  res.send("hello world");
-});
 
 app.get("/init-bot", async (req, res) => {
   const role: string = req.query.role as string;

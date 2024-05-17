59989cdb98434aa8f5c2964b02b44c008170b039
Hassan Yusuf <mhyyusuf3@gmail.com>
2024-05-18 03:15:22 +0530

    Handle multiple websocket clients and identify host and guest of same room

--- src/controllers/websocket.ts
+++ src/controllers/websocket.ts
@@ -2,36 +2,44 @@ import WebSocket from 'ws';
 import fs from 'fs';
 import path from 'path';
 import { talkToGpt, textToSpeech, transcribeSpeechAndPushToHistory } from './openai';
+import url from 'url';
+import IWebSocketClient from '../types/IWebSocketClient';
 
 export let webSocket: WebSocket;
 
-let clients: WebSocket[] = [];
+let clients: IWebSocketClient[] = [];
 
 const startWebSocketServer = () => {
     const wss = new WebSocket.Server({ port: 8081 });
 
-    wss.on('connection', (ws) => {
+    wss.on('connection', (ws, req) => {
         webSocket = ws;
         console.log("client connected!", clients.length);
-        clients.push(ws);
+        const {query: { token, role }} = url.parse(req.url ?? '', true);
+        clients.push({ws, token: token as string, role: role as 'host' | 'guest'});
 
-        ws.addEventListener("message", async (event) => {
-            const speechFile = path.resolve("./test.mp3");
-            fs.writeFileSync(speechFile, Buffer.from(event.data as Buffer), 'binary');
-            await transcribeSpeechAndPushToHistory();
-            const textResponse = await talkToGpt();
-            if(textResponse) {
-                const res = await textToSpeech(textResponse);
-                const receiver = clients.find((c) => c !== ws);
-                if(res && receiver) {
-                    receiver.send(res);
-                    console.log("Sent audio message to client");
-                }
-            };
+        ws.addEventListener("message", async () => {
+            try {
+                await transcribeSpeechAndPushToHistory();
+                const textResponse = await talkToGpt();
+                if(textResponse) {
+                    const res = await textToSpeech(textResponse);
+                    const senderClient = clients.find((c) => c.ws === ws && c.role === 'guest');
+                    if(!senderClient) throw new Error("Host client supposed to receive audio from candidate, not send");
+                    const receiverClient = clients.find((c) => c.ws !== ws && c.token === senderClient?.token);
+                    if (!receiverClient) throw new Error("Receiver client not found!");
+                    if(res && receiverClient) {
+                        receiverClient.ws.send(res);
+                        console.log("Sent audio message to client");
+                    }
+                };
+            } catch (error) {
+                console.error("Error in sending audio to webSocket client:", error);
+            }
         });
         
         ws.on('close', () => {
-            clients = clients.filter(client => client !== ws);
+            clients = clients.filter(client => client.ws !== ws);
             console.log('Client disconnected');
         });
     });

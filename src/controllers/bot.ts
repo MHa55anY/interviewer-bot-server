17726853b5bf6182b7944a757f0a3f52c34400e7
Hassan Yusuf <mhyyusuf3@gmail.com>
2024-05-18 13:51:59 +0530

    Minor template changes

--- src/controllers/bot.ts
+++ src/controllers/bot.ts
@@ -1,26 +1,24 @@
 import puppeteer, { ElementHandle } from "puppeteer";
+import { startRecording, stopRecording } from "./100ms";
 
 //EDIT template according to client code
-const startBot = async () => {
+const startBot = async (roomId:string, roomCodeForHost:string) => {
     const browser = await puppeteer.launch({
-        args: ['--use-fake-ui-for-media-stream'], // Use fake UI for media stream
+      headless:true,
+      args: ['--use-fake-ui-for-media-stream'], // Use fake UI for media stream
     });
     const page = await browser.newPage();
-    await page.goto(`http://localhost:3001?roomcode=rqv-crdr-lrl`);
+    await page.goto(`http://localhost:3001?roomcode=${roomCodeForHost}&role=host`);
 
-      // Wait for the iframe to be loaded - For removing the error screen in dev mode
+    // Wait for the iframe to be loaded - For removing the error screen in dev mode
     const iframe = await page.waitForSelector('iframe#webpack-dev-server-client-overlay') as ElementHandle<HTMLIFrameElement>;
     const frame = await iframe.contentFrame();
 
     const button = await frame.waitForSelector('button');
     await button?.click();
 
-    await page.waitForSelector('#name');
-    await page.type('#name', 'Bot');
-    // await page.type('#roomCode', 'rqv-crdr-lrl');
-
-    await page.waitForSelector('#submit');
-    await page.click('#submit');
+    await startRecording(roomId);
+    setTimeout(() => stopRecording(roomId), 120000);
 };
 
 export default startBot;
